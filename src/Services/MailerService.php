52b1af6c1059732618bc1d7962d14ed15692604e
vrerabek <vrerabek@gmail.com>
2022-07-02 15:02:37 +0200

    added php 8.0 support

---
+++ src/Services/MailerService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Webinity\Framework\Services;
+
+class MailerService extends Service
+{
+
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+
+    public function middleware()
+    {
+        // TODO: Implement middleware() method.
+    }
+
+    public function load()
+    {
+        // TODO: Implement load() method.
+    }
+}
